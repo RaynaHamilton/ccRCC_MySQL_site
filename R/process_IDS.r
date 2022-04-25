1719091670155a3832584200c0df429e391aceab
Rayna Hamilton <raynah314@gmail.com>
2022-04-25 14:15:46 -0400

    database_almost_done

--- R/process_IDS.r
+++ R/process_IDS.r
@@ -7,15 +7,21 @@ library("hgu133plus2.db")
 #this is similar to how our database will be structured!
 probes<-as.character(colnames(renal))
 genes <- select(hgu133plus2.db,keys=substr(probes,2,50), columns=c("SYMBOL", "GENENAME","ENTREZID"),keytype="PROBEID")
-write.table(genes,file="../data/probes_to_genes.tsv",quote=F,row.names=F)
+write.table(genes,file="../data/probes_to_genes.tsv",quote=F,row.names=F,sep="\t")
 
 library("GO.db")
 go_terms<-select(hgu133plus2.db,keys=unique(genes$SYMBOL), columns=c("GO"),keytype="SYMBOL")
 
-write.table(go_terms[,c("SYMBOL","GO")],"../data/genes_to_GO.tsv",quote=F,row.names=F)
+write.table(go_terms[,c("SYMBOL","GO")],"../data/genes_to_GO.tsv",quote=F,row.names=F,sep="\t")
 
 go_names<-Term(unique(go_terms$GO))
 go_names<-cbind(c(names(go_names)),go_names)
-write.table(go_names,"../data/GO_to_names.tsv",quote=F,row.names=F)
+write.table(go_names,"../data/GO_to_names.tsv",quote=F,row.names=F,sep="\t")
 
 
+print(paste(length(unique(genes$PROBEID)),length(unique(genes$SYMBOL)),length(unique(genes$GENENAME))))#"54675 21970 21958" - many-to-one relationship between probe ids and gene symbols, almost all gene symbols have a unique gene name
+go_names<-data.frame(go_names)
+print(paste(length(unique(go_names$V1)),length(unique(go_names$go_names)))) #"18264 18264" - one-to-one relationship between GO ids and names
+
+temp<-genes[genes$PROBEID %in% names(table(genes$PROBEID)[table(genes$PROBEID)>1]),]
+length(unique(temp$PROBEID))#2228 probe ids map to multiple gene symbols (too many to ignore)
--- make_database.py
+++ make_database.py
@@ -9,19 +9,50 @@ def main():
 	probe=[]
 	expression=dict()
 	for i,line in enumerate(open("data/Renal_GSE53757.csv")):
-		temp=line.split(",")
+		temp=line.strip().split(",")
 		if len(temp)>0:
 			if i==0:
 				probe=temp[2:]
 			else:
 				expression[int(temp[0])]=[float(val) for val in temp[2:]]
 				sample[int(temp[0])]=temp[1]
-	print(sample,probe)
+	#print(sample,probe)
+	
+	probes_to_genes=dict()
+	genes_to_names=dict()
+	for line in open("data/probes_to_genes.tsv"):
+		temp=line.strip().split("\t")
+		if len(temp)==4:
+			if temp[0] not in probes_to_genes.keys():#ideally each probe would map to a single gene, but unfortunately this is not true for ~2000 of them
+				probes_to_genes[temp[0]]=[temp[1]]
+			else:
+				probes_to_genes[temp[0]].append(temp[1])
+			genes_to_names[temp[1]]=temp[2] #1-to-1 relationship between gene symbols and names
+			
+			
+	genes_to_go=dict()
+	for line in open("data/genes_to_GO.tsv"):
+		temp=line.strip().split("\t")
+		if len(temp)==2:
+			if temp[0] not in genes_to_go.keys():
+				if temp[1]!="NA":
+					genes_to_go[temp[0]]=[temp[1]] #use a list as we expect a many-to-many relationship between genes and go ids
+			else:
+				genes_to_go[temp[0]].append(temp[1])
+	
+	go_to_names=dict()
+	for line in open("data/GO_to_names.tsv"):
+		temp=line.strip().split("\t")
+		if len(temp)==2:
+			go_to_names[temp[0]]=temp[1]
+			
+	#print(sample,probe,expression,probes_to_genes,genes_to_names,genes_to_go)
+	#print(probes_to_genes,genes_to_names,genes_to_go)
 	conn = mysql.connector.connect(user='rhamil32', password='@101@9+1z',
 	host='localhost', database='rhamil32')
 	
 	curs = conn.cursor()
-	#search featureprop.value for matching gene symbols or gene products, ignoring case:
+	
 	qry = 'show tables;'
 	curs.execute(qry)#curs.execute(qry, ("%"+product.lower()+"%",))
 	temp=[table[0] for table in curs]
@@ -33,7 +64,22 @@ def main():
 		qry="create table "+table+"("+cols+");"
 		print(qry)
 		curs.execute(qry)
-	
+		
+	for sample_id,class_id in sample.items():
+		curs.execute("insert into Sample values (%s,%s);",(sample_id,class_id))
+		#curs.execute('select * from Sample;')
+		#print([value for value in curs])
+	for sample_id,value_list in expression.items():
+		for i,value in enumerate(value_list):
+			curs.execute("insert into Expression values (%s,%s,%s);",(sample_id,probe[i],value))
+	for symbol,name in genes_to_names.items():
+		curs.execute("insert into Gene values (%s,%s);",(symbol,name))
+	for gene,go_id_list in genes_to_go.items():
+		for go_id in go_id_list:
+			curs.execute("insert into Gene_ontology values (%s,%s);",(gene,go_id))
+	for go_id,go_term in go_to_names.items():
+		curs.execute("insert into Ontology_term values (%s,%s);",(go_id,go_term))
+	conn.commit()
 	curs.close()
 	conn.close()
 
